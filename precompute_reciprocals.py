14c6e7e144bb465b60cbd2099ef7d0b41649a392
Chubak Bidpaa <chubakbidpaa@gmail.com>
2023-05-05 04:54:17 +0330

    precomputer fully done

--- precompute_reciprocals.py
+++ precompute_reciprocals.py
@@ -1,80 +1,90 @@
 LOG2_LUT = [
- -1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
+    -1, 0, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3,
     4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
     7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7
 ]
 
 SIXTEENP2 = 65536
 
-def to_binary(decimal: int) -> list[int]:
-	dec = type(
-		"Decimal", (), 
-		{
-			"num": [decimal], 
-			"rems": [], 
-			"__call__": lambda self: list(map(lambda x: x(), [lambda self=self: self.rems.append(self.num[0] & 1), lambda self=self: self.num.__setitem__(0, self.num[0] // 2)])),
-			"__invert__": lambda self: self.num[0] > 0
-		}
-		)()
 
-	while ~dec: dec()
-	dec.rems.reverse()
-	return dec.rems
+def to_binary(decimal: int) -> list[int]:
+    dec = type(
+        "Decimal", (),
+        {
+            "num": [decimal],
+            "rems": [],
+            "__call__": lambda self: list(map(lambda x: x(), [lambda self=self: self.rems.append(self.num[0] & 1), lambda self=self: self.num.__setitem__(0, self.num[0] // 2)])),
+            "__invert__": lambda self: self.num[0] > 0
+        }
+    )()
+
+    while ~dec:
+        dec()
+    dec.rems.reverse()
+    return dec.rems
 
 
 def fast_expontentiation(num: int, exp: int) -> int:
-	binary = to_binary(exp)
-	final_exp = num
-	for b in binary[1:]:
-		final_exp *= final_exp 
-		if b == 1: final_exp *= num
-	return final_exp
-
+    binary = to_binary(exp)
+    final_exp = num
+    for b in binary[1:]:
+        final_exp *= final_exp
+        if b == 1:
+            final_exp *= num
+    return final_exp
 
 def log2n(num: int) -> int:
-	return 24 + LOG2_LUT[num >> 24] if num >> 24 else 16 + LOG2_LUT[num >> 16] if num >> 16 else 8 + LOG2_LUT[num >> 8] if num >> 8 else LOG2_LUT[num]
+    return 24 + LOG2_LUT[num >> 24] if num >> 24 else 16 + LOG2_LUT[num >> 16] if num >> 16 else 8 + LOG2_LUT[num >> 8] if num >> 8 else LOG2_LUT[num]
 
 
 def precompute_reciprocal_single(divisor: int) -> int:
-	l = log2n(divisor) + 1
-	m = (SIXTEENP2 * (fast_expontentiation(2, l) - divisor) // divisor) + 1
-	sh1 = 1 if l > 0 else l
-	sh2 = l - sh1
-	return (sh1 & 0xffff) | ((sh2 & 0xffff) << 16) | ((m & 0xffff) << 32)
+    l = log2n(divisor) + 1
+    m = (SIXTEENP2 * (fast_expontentiation(2, l) - divisor) // divisor) + 1
+    sh1 = 1 if l > 0 else l
+    sh2 = l - sh1
+    return (sh1 & 0xffff) | ((sh2 & 0xffff) << 16) | ((m & 0xffff) << 32)
+
 
 def test_fast_division(num: int, divisor: int):
-	reciprocals = precompute_reciprocal_single(divisor)
-	sh1 = reciprocals & 0xffff
-	sh2 = (reciprocals >> 16) & 0xffff
-	m = (reciprocals >> 32) & 0xffff
-	t1 = (((m * num) >> 16 ) & 0xffff)
-	q = (t1 + ((num - t1) >> sh1)) >> sh2
-	assert q == num // divisor, f"Test failed, {q} != {num // divisor}"
+    reciprocals = precompute_reciprocal_single(divisor)
+    sh1 = reciprocals & 0xffff
+    sh2 = (reciprocals >> 16) & 0xffff
+    m = (reciprocals >> 32) & 0xffff
+    t1 = (((m * num) >> 16) & 0xffff)
+    q = (t1 + ((num - t1) >> sh1)) >> sh2
+    assert q == num // divisor, f"Test failed, {q} != {num // divisor}"
+
 
 def generate_reciprocal_range(range=range(0, 2000)) -> list[int]:
-	return [precompute_reciprocal_single(i) for i in range]
+    return [precompute_reciprocal_single(i) for i in range]
 
-def format_reciprocal_range(range: list[int], split=8) -> list[int]:
-	return "\n".join([f".long {', '.join(portion)}" for portion in [range[i:i+split] for i in range(0, len(range), split)]])
+
+def format_reciprocal_range(rng: list[int], split=8) -> list[int]:
+    return "\n".join([f".long {', '.join(list(map(hex, portion)))}" for portion in [rng[i:i+split] for i in range(0, len(rng), split)]])
 
 
 if __name__ == "__main__":
-	from sys import argv, executable
+    from sys import argv, executable
 
-	def parse_or_default(arg: str, patt: list[str], default: any, fn=int):
-		return fn(arg.split("=")[-2]) if any([arg.startswith(s) for s in patt]) else default
+    def parse_or_default(arg: str, patt: list[str], default: any, fn=int):
+        return fn(arg.split("=")[-2]) if any([arg.startswith(s) for s in patt]) else default
 
-	def print_and_exit(msg: str):
-		print(msg)
-		exit(1)
-	
-	for arg in argv:
-		start = parse_or_default(arg, ["--start", "-s"], 0)
-		end = parse_or_default(arg, ["--end", "-e"], 1000)
-		outp = parse_or_default(arg, ["--out", "-o"], "reciprocals.S", str)
-		_ = parse_or_default(arg, ["--help", "-h"], f"{executable.split('/')[-1]} {argv[0]} [--start/-s]=[0] [--end/-e]=[1000] [--start/-s]=[reciprocals.S]", print_and_exit)
+    def print_and_exit(msg: str):
+        print(msg)
+        exit(1)
 
+    HELP_STRING = f"{executable.split('/')[-1]} {argv[0]} [--start/-s]=[0] [--end/-e]=[1000] [--start/-s]=[reciprocals.S] [--sectname/-sn]=[.section reciprocals, \"a\", @progbits] [--perline/-pl]=[2]"
 
-	rangerpc = generate_reciprocal_range(range(start, end))
-	formatted = format_reciprocal_range(rangerpc)
-	with open(outp, "w") as fw: fw.write(f".section reciprocals, r\n{formatted}")
+    for arg in argv:
+        start = parse_or_default(arg, ["--start", "-s"], 1)
+        end = parse_or_default(arg, ["--end", "-e"], 1001)
+        outp = parse_or_default(arg, ["--out", "-o"], "reciprocals.S", str)       
+        sectname = parse_or_default(arg, ["--sectname", "-sn"], '.section reciprocals, "a", @progbits', str)
+        perline = parse_or_default(arg, ["--perline", "-pl"], 2)
+        _ = parse_or_default(
+            arg, ["--help", "-h"], HELP_STRING, print_and_exit)
+
+    rangerpc = generate_reciprocal_range(range(start, end))
+    formatted = format_reciprocal_range(rangerpc, perline)
+    with open(outp, "w") as fw:
+        fw.write(f'{sectname}\n{formatted}')
